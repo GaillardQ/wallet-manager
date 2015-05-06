6cf989aa6aca343abec371f06e3dd11a6b526d65
Quentin <quentin.gaillard.4@gmail.com>
2015-05-06 15:27:58 +0000

    Update master to be ready for the prod

--- routes/login.js
+++ routes/login.js
@@ -9,13 +9,11 @@ var sess;
 router.get('/', function(req, res, next) {
     sess = req.session;
     var error = req.query.error;
-    console.log("ERROR : "+error);
     res.render('common/login.html.twig', {error: error});
 });
 
 router.post('/check', function(req, res, next) {
     var login = req.body.login;
-    console.log(md5);
     var hashed_login = md5(md5(login) + md5(salt));
     var comp_login = md5(md5(l) + md5(salt));
 
